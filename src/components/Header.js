17ff92a89cfeb778d04d7b0388e51b557124f74d
Jas <c.jasminher@gmail.com>
2019-06-12 22:48:40 -0400

    add: Added navbar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,16 @@
 //header with the title corresponding to the content will go here
+
+
+import React from 'react';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+    <h2 className="header">Name of Page</h2>
+      
+    
+    )
+  }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -5,36 +5,43 @@ import './index.css';
 import App from './App';
 import Campuses from './components/Campuses';
 import Students from './components/Students';
-
-
+import Header from './components/Header';
 
 
 const routing = (
+
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/components/Campuses">Campuses</Link>
-          </li>
-          <li>
-            <Link to="/components/Students">Students</Link>
-          </li>
-        </ul>
+        <Header />
+
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarToggler">
+
+                <ul className="navbar-nav justify-content-end">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/components/Campuses">Campuses</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/components/Students">Students</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
         <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/components/Campuses" component={Campuses} />
-          <Route path="/components/Students" component={Students} />
-          <Route path=""   
-          
-          
-          component={Campuses} />
+            <Route exact path="/" component={App} />
+            <Route path="/components/Campuses" component={Campuses} />
+            <Route path="/components/Students" component={Students} />
         </Switch>
-      </div>
+
     </Router>
-  )
+)
 
 
 
