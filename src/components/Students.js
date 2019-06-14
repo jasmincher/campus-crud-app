969d72654e6699dc9213249b7d3672dcfd396adb
Jas <c.jasminher@gmail.com>
2019-06-14 01:04:05 -0400

    add: Added styling to heading for students

--- src/components/Students.js
+++ src/components/Students.js
@@ -2,13 +2,19 @@
 import React from 'react';
 import StudentCard from './StudentCard';
 import profile from '../images/profile.jpg';
-
+import AddButton from './AddButton';
+import '../ CSS/Students.css';
 
 
 const Students = () => (
 
 
     <div>
+        <div>
+        <h2 className="top">All Students</h2>
+        <AddButton name="Add Student" className="button"/>
+        </div>
+      
         <StudentCard studentName="Jane Doe" campusName="Lehman College" img={profile} />
         <StudentCard studentName="John Doe" campusName="Lehman College" img={profile} />
         <StudentCard studentName="Jack Doe" campusName="Lehman College" img={profile} />
