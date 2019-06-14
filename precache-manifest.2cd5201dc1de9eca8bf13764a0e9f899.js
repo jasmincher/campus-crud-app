3c2967b64cbc702156840e1211703ecbdec36eb0
Jas <c.jasminher@gmail.com>
2019-06-13 23:37:46 -0400

    Updates

---
+++ precache-manifest.2cd5201dc1de9eca8bf13764a0e9f899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98de73045fc1543cd1c49f9884849d9",
+    "url": "/campus-crud-app/index.html"
+  },
+  {
+    "revision": "f2022c10adee304f1430",
+    "url": "/campus-crud-app/static/css/main.c22a02b3.chunk.css"
+  },
+  {
+    "revision": "17778a8f282a9b7c0510",
+    "url": "/campus-crud-app/static/js/2.8a2a16d6.chunk.js"
+  },
+  {
+    "revision": "f2022c10adee304f1430",
+    "url": "/campus-crud-app/static/js/main.9ce70e2f.chunk.js"
+  },
+  {
+    "revision": "668ec3fb942034dcd839",
+    "url": "/campus-crud-app/static/js/runtime~main.25ef9c66.js"
+  }
+]);
