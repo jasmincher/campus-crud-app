3c1d369d2215737e13de6722ae27ed7c1eaf53ee
Jas <c.jasminher@gmail.com>
2019-06-13 23:48:50 -0400

    add: Added links to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 
 const Home = () => (
     <div>
-    This is home
+      
+    To checkout the other pages go to:
+
+    <Link className="nav-link" to="/New Student Form">New Student Form</Link>
+    <Link className="nav-link" to="/Blank Slate Students">Blank Slate Students</Link>
+
+
+   
     </div>
   )
 
