18a5dc91ca498a7f5e1ddcf6be5d8940cd33385d
Geidy <geidydm@gmail.com>
2019-06-14 18:21:15 -0400

    updated route path to new files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Students from './Students';
-import Campuses from './Campuses';
+import Campus from './Campus';
 import Home from './Home';
 import StudentNewForm from './StudentNewForm';
+import CampusNewForm from './CampusNewForm';
 import BlankSlateStudents from './BlankSlateStudents';
+import BlankSlateCampus from './BlankSlateCampus';
 
 
 const Main = () => (
@@ -12,9 +14,11 @@ const Main = () => (
     <Switch>
       <Route path='/Home' component={Home}/>
       <Route path='/Students Listing' component={Students}/>
-      <Route path='/Campuses Listing' component={Campuses}/>
+      <Route path='/Campus Listing' component={Campus}/>
       <Route path='/New Student Form' component={StudentNewForm}/>
+      <Route path='/New Campus Form' component={CampusNewForm}/>
       <Route path='/Blank Slate Students' component={BlankSlateStudents}/>
+      <Route path='/Blank Slate Campus' component={BlankSlateCampus}/>
     </Switch>
 
 )
